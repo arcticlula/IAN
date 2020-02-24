edd0135f033420d6b10ef67e915daf28c9fd895f
Maria <maria.almeida@dotlogic.pt>
2020-02-24 14:39:19 +0000

    merge

--- app/ian/src/store/index.js
+++ app/ian/src/store/index.js
@@ -5,7 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    bluetooth: { state: "", devices: { saved: [], unpaired: [] } },
+    bluetooth: { state: "", devices: { saved: [{
+      "class": 276,
+      "id": "10:BF:48:CB:00:00",
+      "address": "10:BF:48:CB:00:00",
+      "name": "Nexus 7"
+  }, {
+      "class": 7936,
+      "id": "00:06:66:4D:00:00",
+      "address": "00:06:66:4D:00:00",
+      "name": "RN42"
+  }], unpaired: [] } },
     patterns: { shape: "circle", div: 4, orientation: 0, optionsShape: { circle: ["Normal", "Gradiente"], square: ["sq"], triangle: ["tr"] }, optionsDiv: { circle: { "Normal": [2, 3, 4, 5, 6, 7, 8, 9, 10], "Gradiente": [2, 3, 4, 5, 6, 7, 8, 9, 10] }, square: ["sq"], triangle: ["tr"], star: ["st"] }, selected: "" }
   },
   getters: {
